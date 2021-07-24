92e8c76c92c5ba21ad18ad71f9b9e7fc730528f3
sigma13-K <sigma13@kaist.ac.kr>
2021-07-25 06:19:14 +0900

    fix error

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,7 @@ module.exports = {
       {
         rootPathPrefix: '~',
         rootPathSuffix: 'src',
-      }
-    ]
-  ]
+      },
+    ],
+  ],
 };
--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 // import App from './App';
-import App from '~/App';    // babel-plugin-root-import usage ~
+import App from './src/App';    // babel-plugin-root-import usage ~
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
